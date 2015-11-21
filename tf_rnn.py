0d8595e742cc620389c5478faeb99b5f6a800cd6
Ubuntu <ubuntu@ip-172-31-11-117.us-west-1.compute.internal>
2015-11-21 18:08:06 +0000

    more

--- tf_rnn.py
+++ tf_rnn.py
@@ -7,6 +7,8 @@ rng = np.random.RandomState(42)
 dim_input = 11
 dim_hidden = 128
 
+MAX_SEQ_LENGTH = 15
+
 # embed each input as a vector
 W_in = tf.Variable(rng.uniform(-.1, .1,
 	(dim_input, dim_hidden)).astype('float32'))
@@ -18,4 +20,7 @@ W_rec = tf.Variable(rng.uniform(-.1, .1,
 
 # recurrence step
 def recurrence(inp, state):
-    
+   return tf.tanh(tf.matmul(inp, W_in) + tf.matmul(state, W_rec))
+
+
+
